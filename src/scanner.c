779faee9133406f9faafabb1d9e66baaa2e3307c
urbit-pilled <129720764+urbit-pilled@users.noreply.github.com>
2024-03-22 16:54:41 +1300

    fix: fix external scanner (#12)

--- src/scanner.c
+++ src/scanner.c
@@ -5,7 +5,7 @@ void *tree_sitter_gritql_external_scanner_create() {
   return NULL;
 }
 
-bool tree_sitter_xml_external_scanner_scan(void *payload, TSLexer *lexer, const bool *valid_symbols) {
+bool tree_sitter_gritql_external_scanner_scan(void *payload, TSLexer *lexer, const bool *valid_symbols) {
   return false;
 }
 
